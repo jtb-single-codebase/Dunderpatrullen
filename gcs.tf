fd71cd797096d0303d8a69e2e0fe50692dec0d5e
bd-jbaveras <jbaveras@blackduck.com>
2026-08-07 13:57:04 +0200

    update file gcs.tf

--- gcs.tf
+++ gcs.tf
@@ -12,10 +12,4 @@ resource "google_storage_bucket" "terragoat_website" {
     git_repo             = "terragoat"
     yor_trace            = "bd00cd2e-f53f-4daf-8d4d-74c47846c1cc"
   }
-}
-
-resource "google_storage_bucket_iam_binding" "allow_public_read" {
-  bucket  = google_storage_bucket.terragoat_website.id
-  members = ["allUsers"]
-  role    = "roles/storage.objectViewer"
-}
+}
